fe78bae341d226bc40d99ee744a29d380c4ad03a
InvertedOwl <70726294+InvertedOwl@users.noreply.github.com>
2022-10-14 12:23:29 -0600

    Added methods for ports and props

--- Plasma-Custom-Nodes/Program.cs
+++ Plasma-Custom-Nodes/Program.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using Behavior;
+using HarmonyLib;
 using System.Reflection;
 using UnityEngine;
 
@@ -51,6 +52,36 @@ namespace PlasmaModding {
             if ( gestalt.ports == null)
                 gestalt.ports = new Dictionary<int, AgentGestalt.Port>();
             RegisterGestalt(gestalt);
+        }
+
+        public static AgentGestalt.Port CreatePort(string name, string desc, AgentGestalt.Port.Types type, Data.Types datatype, int pos, int operation = -1)
+        {
+            AgentGestalt.Port port = new AgentGestalt.Port();
+            port.name = name;
+            port.description = desc;
+            port.type = type;
+            port.dataType = datatype;
+            port.position = pos;
+
+            if (operation > -1)
+            {
+                port.operation = operation;
+            }
+
+
+            return port;
+        }
+
+        public static AgentProperty CreateProperty(string name, string desc, Data defaultData, int driverCommand, int handler, int position, bool configurable)
+        {
+            AgentProperty prop = new AgentGestalt.Property();
+            prop.configurable = configurable;
+            prop.defaultData = defaultData;
+            prop.description = desc;
+            prop.driverCommand = driverCommand;
+            prop.handler = handler;
+            prop.name = name;
+            prop.position = position;
         }
 
         [HarmonyPatch(typeof(Resources), "LoadAll", new Type[] {typeof(string), typeof(Type)})]
