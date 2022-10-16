33d5e83e65f6809f91ce77d6c65e707ddcb308d8
hippolippo <joelhaftel@gmail.com>
2022-10-16 02:57:50 -0400

    Removed debug print messages

--- Plasma-Custom-Nodes/CustomAgent.cs
+++ Plasma-Custom-Nodes/CustomAgent.cs
@@ -20,25 +20,10 @@ namespace PlasmaModding
         }
 
         internal static Dictionary<Type,Dictionary<string, int>> outputs = new Dictionary<Type,Dictionary<string, int>>();
-        /*
-        protected static void WriteOutput(string name, Data data, SketchNode node)
-        {
-            node.ports[outputs[MethodBase.GetCurrentMethod().DeclaringType][name]].Commit(data);
-        }*/
 
         protected void WriteOutput(string name, Data data)
         {
-            Debug.Log("1");
-            var a = currentSketchNode.ports;
-            Debug.Log("2");
-            var b = outputs[this.GetType()];
-            Debug.Log("3");
-            var c = b[name];
-            Debug.Log("4");
-            var d = a[c];
-            Debug.Log("5");
-            d.Commit(data);
-            //currentSketchNode.ports[outputs[this.GetType()][name]].Commit(data);
+            currentSketchNode.ports[outputs[this.GetType()][name]].Commit(data);
         }
     }
 }
